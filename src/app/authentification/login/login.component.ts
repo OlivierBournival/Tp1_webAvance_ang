22e3515d82500975f8a6e984014e5835cd3e293d
Sourack <sourack@galactica-mc.net>
2023-10-05 20:34:04 -0400

    FCT Amélioration des validations sur champs

--- src/app/authentification/login/login.component.ts
+++ src/app/authentification/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginDTO } from 'src/app/DTO/LoginDTO';
 import { AuthentificationService } from 'src/app/services/authentification.service';
 
- interface LoginData {
-  Email?: string | null ;
-  Password?: string | null ;
+interface LoginData {
+  email?: string;
+  password?: string;
 }
 
 @Component({
@@ -18,47 +24,28 @@ export class LoginComponent implements OnInit {
   hidePassword = true;
   loginDTO: LoginDTO = new LoginDTO('', '');
   message: string = '';
-  LoginForm:FormGroup<any>;
-  QData:LoginData | null = null;
-
+  LoginForm: FormGroup<any>;
+  QData: LoginData | null = null;
 
   constructor(
     public authentificationService: AuthentificationService,
     public router: Router,
     private fb: FormBuilder
   ) {
-
-    this.LoginForm = this.fb.group({
-       Email: ['', [Validators.required, Validators.email]],
-      password: ['',[Validators.required, Validators.min(3), Validators.max(30)]],
-    }, {validators: this.LoginValidator});
-
+    this.LoginForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [Validators.required, Validators.min(3), Validators.max(30)],
+        ],
+      },
+    );
 
     this.LoginForm.valueChanges.subscribe(() => {
       this.QData = this.LoginForm.value;
     });
   }
-
-  LoginValidator(control: AbstractControl): ValidationErrors | null {
-   const Email = control.get('Email')?.value;
-   const password = control.get('Password')?.value;
-   let atLeastOneMistake:boolean = false;
-
-
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (!emailRegex.test(Email)) {
-      control.get('Email')?.setErrors({ invalidEmail: true });
-      atLeastOneMistake = true;
-    }
-   // if (password == null) {
-    //  control.get('password')?.setErrors({ passwordlength: true });
-   //   atLeastOneMistake = true;
-  //  }
-    
-  
-   return atLeastOneMistake?{atLeastOneMistake:true}:null;
-  }
-
 
   ngOnInit() {}
 
--- src/app/authentification/register/register.component.ts
+++ src/app/authentification/register/register.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { RegisterDTO } from 'src/app/DTO/RegisterDTO';
 import { AuthentificationService } from 'src/app/services/authentification.service';
 
-
 interface LoginData {
-  Email?: string | null ;
-  Password?: string | null ;
-  PasswordConfirm?: string| null }
+  email?: string;
+  password?: string;
+  passwordConfirm?: string;
+}
 
 @Component({
   selector: 'app-register',
@@ -19,51 +25,47 @@ export class RegisterComponent implements OnInit {
   hidePassword = true;
   hideConfirmation = true;
   registerDTO: RegisterDTO = new RegisterDTO('', '', '');
-  RegisterForm:FormGroup<any>;
-  RegisterData:LoginData | null = null;
+  RegisterForm: FormGroup<any>;
+  RegisterData: LoginData | null = null;
   OValidator: any;
-  
+
   constructor(
     public authentificationService: AuthentificationService,
     public router: Router,
     private fb: FormBuilder
   ) {
+    this.RegisterForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [Validators.required, Validators.min(3), Validators.max(30)],
+        ],
+        passwordConfirm: [
+          '',
+          [Validators.required, Validators.min(3), Validators.max(30)],
+        ],
+      },
+      { validators: this.RegisterValidator }
+    );
 
-    
-    this.RegisterForm = this.fb.group({
-      Email: ['', [Validators.required, Validators.email]],
-      password: ['',[Validators.required, Validators.min(3), Validators.max(30)]],
-      passwordConfirm: ['', [Validators.required, Validators.min(3), Validators.max(30)]],
-   }, {validators: this.RegisterValidator});
-
-
-   this.RegisterForm.valueChanges.subscribe(() => {
-     this.RegisterData = this.RegisterForm.value;
-   }); 
- }
-
- RegisterValidator(control: AbstractControl): ValidationErrors | null {
-  const Email = control.get('Email')?.value;
-  const password = control.get('password')?.value;
-  const passwordConfirm = control.get('passwordConfirm')?.value;
-  let atLeastOneMistake:boolean = false;
-
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-  if (!emailRegex.test(Email)) {
-    control.get('Email')?.setErrors({ invalidEmail: true });
-    atLeastOneMistake = true;
+    this.RegisterForm.valueChanges.subscribe(() => {
+      this.RegisterData = this.RegisterForm.value;
+    });
   }
 
+  RegisterValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const passwordConfirm = control.get('passwordConfirm')?.value;
 
     if (password != passwordConfirm) {
-      control.get('passwordConfirm')?.setErrors({passwordConfirmEqualsPassword:true});
-      atLeastOneMistake = true;
-   
+      control
+        .get('passwordConfirm')
+        ?.setErrors({ passwordConfirmEqualsPassword: true });
     }
-    return atLeastOneMistake?{atLeastOneMistake:false}:null;
 
-}
+    return null;
+  }
 
   ngOnInit() {}
 
